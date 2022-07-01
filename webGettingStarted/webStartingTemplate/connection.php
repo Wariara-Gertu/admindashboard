2a6a60e8953a76ec8de98e9440b862cec5377554
Wariara-Gertu <riarak32@gmail.com>
2022-07-01 13:49:28 +0300

    Feat added student file

---
+++ webGettingStarted/webStartingTemplate/connection.php
@@ -0,0 +1,21 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "zalego";
+
+$conn=mysqli_connect($server,$username,$password,$database);
+if($conn)
+{
+    echo "Database connection success";
+}
+else{
+    echo "Error occured" .mysqli_error($conn);
+}
+
+
+
+
+
+
+?>
