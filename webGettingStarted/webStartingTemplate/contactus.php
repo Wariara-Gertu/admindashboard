1dfa536de08cb23cee2004c7d51b0599b28ebf40
Wariara-Gertu <riarak32@gmail.com>
2022-07-15 13:32:32 +0300

    FEAT new folder-contact

---
+++ webGettingStarted/webStartingTemplate/contactus.php
@@ -0,0 +1,108 @@
+<?php
+		require_once('contact/connection.php');
+		$sqlQuery = mysqli_query($conn, "SELECT * FROM contactus");
+		
+
+?> 
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+	<?php require_once('includes/headers.php')?>
+
+
+    <body>
+	<!-- All our code. write here   -->
+	<?php require_once('includes/navbar.php')?>
+
+
+	<div class="sidebar">
+	    <?php require_once('includes/sidebar.php')?>
+
+	</div>
+
+	<div class="main-content">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-header bg-dark text-white text-center">
+                        <h4 class="card-title">Contact Us</h4>
+                    </div>
+                    
+                       
+                    <div class="card-body">
+						<table class="table table-striped table-hover table-responsive" style="font-style: 12px;">
+							<thead>
+								<tr>
+									<th>No.</th>
+									<th>First Name</th>
+                                    <th>Last Name</th>
+									<th>Phone Number</th>
+									<th>Email</th>
+									<th>Message</th>
+									<th>Enrolled On</th>
+									
+									
+								</tr>
+							</thead>
+							<tbody>
+								<?php 
+								while($fetchrecords = mysqli_fetch_array($sqlQuery)) { ?>
+								<tr>
+									<td><?php  echo $fetchrecords['no']?></td>
+									<td><?php  echo $fetchrecords['firstname']?></td>
+                                    <td><?php  echo $fetchrecords['lastname']?></td>
+									<td><?php  echo $fetchrecords['phonenumber']?></td>
+									<td><?php  echo $fetchrecords['email']?></td>
+									<td><?php  echo $fetchrecords['message']?></td>
+									<td><?php  echo $fetchrecords['created_at']?></td>
+									
+									
+								</tr>
+
+
+								<?php }?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		
+	
+
+
+</div>
+</div>
+	
+<?php require_once('includes/scripts.php') ?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
